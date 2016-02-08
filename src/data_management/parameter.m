f9a3483a8f40876729d2a6195c63b31bb9f8b015
Jan Wickerath <jan.wickerath@mailbox.org>
2016-02-08 16:51:53 +0100

    Changed pathsettings for parameter preparation.

--- src/data_management/parameter.m
+++ src/data_management/parameter.m
@@ -2,19 +2,20 @@
 
 %% Path settings
 
-path_to_model_specs = project_paths('IN_MODEL_SPECS', 'parameters.mat');
-% path_to_model_specs = '../model_specs/parameters.mat';
+path_to_model_specs = project_paths('OUT_DATA', 'parameters.mat');
+% path_to_model_specs = '../../bld/out/data/parameters.mat';
+
 
 %% Set parameters
-param.discount_factor = 0.9825
-param.tax_advantage   = 0.35
-param.alpha           = 1.8834
-param.theta           = 0.36
-param.delta           = 0.025
-param.xi_bar          = 0.1634
-param.kappa           = 0.146
-param.sigma_z         = 0.0045
-param.sigma_xi        = 0.0098
+param.discount_factor = 0.9825;
+param.tax_advantage   = 0.35;
+param.alpha           = 1.8834;
+param.theta           = 0.36;
+param.delta           = 0.025;
+param.xi_bar          = 0.1634;
+param.kappa           = 0.146;
+param.sigma_z         = 0.0045;
+param.sigma_xi        = 0.0098;
 
 %% Save parameters to model specs folder
 save(path_to_model_specs, 'param');
