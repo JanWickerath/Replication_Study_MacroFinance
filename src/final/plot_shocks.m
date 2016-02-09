13725cbbe583549ac81694d919684bee315fc87e
Jan Wickerath <jan.wickerath@mailbox.org>
2016-02-09 11:55:37 +0100

    Added timeline and titles to shock plot.

--- src/final/plot_shocks.m
+++ src/final/plot_shocks.m
@@ -3,24 +3,32 @@
 
 %% Path settings
 
+% path_to_data = project_paths('OUT_DATA', 'dataset.mat');
 % path_to_shocks = project_paths('OUT_ANALYSIS', 'shock_innovations.mat');
 path_to_shocks = '../../bld/out/analysis/shock_innovations.mat';
+path_to_data = '../../bld/out/data/dataset.mat';
 
 %% Load data
 load(path_to_shocks);
+load(path_to_data);
 
 %% Plot results in a 2 by 2 matrix of plots
 
 figure
 % set(gcf, 'visible', 'off')
     subplot(2, 2, 1)
-    plot(productivity_state)
+    plot(timeline.estimation_sample(2:end-1), productivity_state(1:end-1)* ...
+         100)
+    title('Level of productivity, z')
     
     subplot(2, 2, 2)
-    plot(financial_state)
+    plot(timeline.estimation_sample(2:end-1), financial_state(1:end-1)*100)
+    title('Level of financial conditions, \xi')
     
     subplot(2, 2, 3)
-    plot(prod_innovations)
+    plot(timeline.estimation_sample(2:end-1), prod_innovations*100)
+    title('Innvoations to productivity, \epsilon_z')
     
     subplot(2, 2, 4)
-    plot(fin_innovations)
+    plot(timeline.estimation_sample(2:end-1), fin_innovations*100)
+    title('Innovations to financial conditions, \epsilon_{\xi}')
