74cb06de201c98650dc883ec566bdb71f1f46d3a
Jan Wickerath <jan.wickerath@mailbox.org>
2016-02-10 16:57:43 +0100

    Outline for Update of figure 5.

---
+++ src/final/baseline_model_data_plot.m
@@ -0,0 +1,14 @@
+% Compare the model simulation for output, working hours, equity payout and
+% debt repurchases to its observed counterparts as done in figure 5 in the
+% original paper.
+
+%% Path settings
+
+
+%% Load in data
+
+
+%% Plot comparison graphs
+
+
+%% Save graph
